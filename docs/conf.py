60722ee9afbc768a5723c1f2134ae1512fa84f0d
victorycrest <victorycrest1602@gmail.com>
2023-08-12 14:56:57 +0530

    add sphinx config

---
+++ docs/conf.py
@@ -0,0 +1,16 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+project = 'llmbox'
+copyright = '2023, Victory Crest'
+author = 'Victory Crest'
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinxcontrib.napoleon'
+]
+templates_path = ['_templates']
+exclude_patterns = []
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+add_module_names = False
