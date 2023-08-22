56711d1fdcd2d60b662faaf058c6f053de8316e1
victorycrest <victorycrest1602@gmail.com>
2023-08-22 15:09:29 +0530

    add openai creator

--- llmbox/llms/base.py
+++ llmbox/llms/base.py
@@ -1,22 +1,31 @@
-import abc
+from abc import ABC, abstractmethod
 from enum import Enum
 
 
 class LLMCreator(Enum):
-    """
-    List of LLM creators.
-    """
+    """List of LLM creators."""
 
-    ANTHROPIC = 1
+    ANTHROPIC = 'anthropic'
+    OPENAI = 'openai'
 
 
-class BaseLLM(abc.ABC):
+class BaseLLM(ABC):
     """
     Base class for LLMs.
 
     Args:
-        creator(LLMCreator): Creator of LLM
+        creator(:obj:`LLMCreator`): Creator of the LLM
     """
 
-    def __init__(self, creator: LLMCreator):
+    def __init__(self, creator: LLMCreator) -> None:
         self._creator = creator
+
+    @abstractmethod
+    def generate(self, **kwargs):
+        pass
+
+    @property
+    def creator(self) -> str:
+        """str: Creator of the LLM."""
+
+        return self._creator.name
